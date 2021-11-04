86719f31e9d3bb28a7ff44590dc07a36e70b2a55
BorysShvyryd <bormanpgg@gmail.com>
2021-11-04 18:55:22 +0200

    фÐadd EmailService

---
+++ src/main/java/pl/coderslab/charity/entity/pojo/EmailAuthenticator.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity.entity.pojo;
+
+import javax.mail.PasswordAuthentication;
+
+public class EmailAuthenticator extends javax.mail.Authenticator
+{
+    private final String login   ;
+    private final String password;
+    public EmailAuthenticator (final String login, final String password)
+    {
+        this.login    = login;
+        this.password = password;
+    }
+    public PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(login, password);
+    }
+}
