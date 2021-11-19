a68115886d4acac7f052c4ec1813577832a389bf
BorysShvyryd <bormanpgg@gmail.com>
2021-11-19 21:06:02 +0200

    remuve biznesslogic in AdminControler donations

--- src/main/java/pl/coderslab/charity/service/DonationService.java
+++ src/main/java/pl/coderslab/charity/service/DonationService.java
@@ -8,6 +8,7 @@ import pl.coderslab.charity.repository.DonationRepository;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class DonationService {
@@ -55,5 +56,76 @@ public class DonationService {
 
     public Donation getById(Long id) {
         return donationRepository.getById(id);
+    }
+
+    public Stream<Donation> donationsStreemChange(String stream_change, String querySearch) {
+
+        String[] operations = stream_change.split(";");
+
+        Stream<Donation> donationsStream;
+
+        if ("".equals(stream_change))
+            return findAllSortByStatus().stream();
+
+        if (querySearch == null) {
+            donationsStream = findAll().stream();
+        } else {
+            donationsStream = findAllSortByStatus().stream();
+        }
+
+        for (String operation : operations) {
+            switch (operation.split("=")[0]) {
+                case "sort":
+                    switch (operation.split("=")[1]) {
+                        case "institution_up":
+                            donationsStream = donationsStream.sorted((o1, o2) ->
+                                    o1.getInstitution().getName().compareTo(o2.getInstitution().getName()));
+                            break;
+                        case "institution_down":
+                            donationsStream = donationsStream.sorted((o1, o2) ->
+                                    o2.getInstitution().getName().compareTo(o1.getInstitution().getName()));
+                            break;
+                        case "address_up":
+                            donationsStream = donationsStream.sorted((o1, o2) ->
+                                    o1.getStreet().compareTo(o2.getStreet()));
+                            break;
+                        case "address_down":
+                            donationsStream = donationsStream.sorted((o1, o2) ->
+                                    o2.getStreet().compareTo(o1.getStreet()));
+                            break;
+                        case "username_up":
+                            donationsStream = donationsStream.sorted((o1, o2) ->
+                                    (o1.getUser().getName() + o1.getUser().getSurname())
+                                            .compareTo(o2.getUser().getName() + o2.getUser().getSurname()));
+                            break;
+                        case "username_down":
+                            donationsStream = donationsStream.sorted((o1, o2) ->
+                                    (o2.getUser().getName() + o2.getUser().getSurname())
+                                            .compareTo(o1.getUser().getName() + o1.getUser().getSurname()));
+                            break;
+                    }
+
+                case "filter":
+                    switch (operation.split("=")[1]) {
+                        case "username":
+                            donationsStream = donationsStream.filter(o ->
+                                    (o.getUser().getSurname() + o.getUser().getName()).toLowerCase()
+                                            .contains(querySearch.toLowerCase()));
+                            break;
+                        case "institution":
+                            donationsStream = donationsStream.filter(o ->
+                                    o.getInstitution().getName().toLowerCase()
+                                            .contains(querySearch.toLowerCase()));
+                            break;
+                        case "address":
+                            donationsStream = donationsStream.filter(o ->
+                                    o.getStreet().toLowerCase()
+                                            .contains(querySearch.toLowerCase()));
+                            break;
+                    }
+            }
+        }
+
+        return donationsStream;
     }
 }
